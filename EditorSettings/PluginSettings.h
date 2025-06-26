d1b832420a2edfff185ae48281bc762b5e00e6da
TheBeardedBerry <spam@christopherberry.com>
2025-06-26 15:34:53 -0700

    Adding an Editor Settings page example

---
+++ EditorSettings/PluginSettings.h
@@ -0,0 +1,79 @@
+// Written by TheBeardedBerry
+// MIT License
+
+/**
+ * PluginSettings.h
+ * ----------------------------------------------------------
+ * Created: 2025/06/26
+ * Tested: Unreal 5.5
+ * ----------------------------------------------------------
+ * Minimum viable example of a working settings page.
+ *
+**/
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DeveloperSettings.h"              // DeveloperSettings needs to be added to the dependencies list
+#include "WMDEditorValidationSettings.generated.h"
+
+USTRUCT()
+struct FMyStruct
+{
+	GENERATED_BODY()
+
+	// structs can be used in settings as well, you just need to be sure to add the UPROPERTY element to each property you want to be visible in the settings window.
+	UPROPERTY(EditAnywhere)
+	FString Name;
+	UPROPERTY(EditAnywhere)
+	int32 Quantity;
+};
+
+enum class EMyEnum : uint8
+{
+	A,
+	B,
+	C,
+	D
+};
+
+/**
+ * 
+ */
+ 
+/**
+ * config=Editor               defines the section of the project settings page your settings will appear in
+ * defaultconfig               tells the editor to put these settings in the Default____.ini files (the ones that arent distributed to users).
+ * DisplayName="My Plugin"     is the title of the settings page
+ * 
+ * the "meta" argument is a very flexible part of the UCLASS/UPROPERTY/etc system. Epic has written out many of the options you can use in the meta arguement here: https://dev.epicgames.com/documentation/en-us/unreal-engine/API/Runtime/CoreUObject/UObject/UM_3
+ */
+
+UCLASS(config=Editor, defaultconfig, meta=(DisplayName="My Plugin"))
+class MYPLUGIN_API UPluginSettings : public UDeveloperSettings
+{
+	GENERATED_BODY()
+
+	// config tells the system to save the value to the .ini filebuf
+	UPROPERTY(config, EditAnywhere, Category="Category A")
+	int32 MyIntValue;
+	
+	UPROPERTY(config, EditAnywhere, Category="Category A")
+	float MyFloatWithDefaultValue = 32.7f;
+
+	// Subcategories can be defined by adding a pipe "|" between the category and sub-category name (be careful with spelling here when you use the same category between options).
+	UPROPERTY(config, EditAnywhere, Category="Category A|Bool Settings")
+	bool MyBoolValue;
+
+	UPROPERTY(config, EditAnywhere, Category="Category A|Array Settings")
+	TArray<FString> MyStringArray;
+
+	UPROPERTY(config, EditAnywhere, Category="Category for Custom Structs")
+	TArray<FMyStruct>;
+
+	UPROPERTY(config, EditAnywhere, Category="Category B|Custom Enums")
+	EMyEnum MyEnumValue;
+	
+	UPROPERTY(config, EditAnywhere, Category="Category B|Custom Enums")
+	EMyEnum MyEnumWithDefaultValue = EMyEnum::C;
+};
--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # Unreal C++ Reference
+
+I primarily work as a tools focused tech artist but I come from an art/tech art background rather than an engineering one. Unreal and DCC apps tend not to fully document all the features available to develop tools with so I am documenting what I find that works.
+
+# Notes:
+
+- These examples are meant more as a guide than anything, some may compile immediately, some may require some other code/files/structure. If I have time, I may go in and restructure the whole depot as a project that can be loaded/compiled so that its easier to understand in context.
+- I try to document the code as I go, if anyone finds anything confusing feel free to poke me and I will try to clarify.
+- If you have good examples that you think may fit well here, feel free to open a pull request.
